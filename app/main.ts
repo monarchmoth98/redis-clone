bc3b5baa545d477ad092af67cbf02e50ace14c56
Edouard Finet <edouardf@apadmi.com>
2024-09-14 08:14:42 +0100

    pass 2nd stage

--- app/main.ts
+++ app/main.ts
@@ -6,6 +6,7 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
    // Handle connection
+    console.log("Connected to the server");
 });
 
 server.listen(6379, "127.0.0.1");
