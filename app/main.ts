bafd3e9d40cd87495d8730ba2350c272583f46ce
straightfrench <edouard.finet@hotmail.co.uk>
2024-09-15 08:34:03 +0100

    feat: RG2 Implement PING.

--- app/main.ts
+++ app/main.ts
@@ -6,7 +6,15 @@ console.log("Logs from your program will appear here!");
 // Uncomment this block to pass the first stage
 const server: net.Server = net.createServer((connection: net.Socket) => {
    // Handle connection
-    console.log("Connected to the server");
+   console.log("Connected");
+   connection.on('end', () => {
+       console.log("Connection ended");
+   });
+
+   connection.on('data', (data) => {
+      console.log('received: ', data.toString());
+      connection.write('+PONG\r\n');
+   });
 });
 
 server.listen(6379, "127.0.0.1");
